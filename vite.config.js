c058342f5e239e4609302e6dea122be79e106ca8
Dat Tran <dat.tran@ghielectronics.com>
2026-02-06 14:16:10 -0500

    change config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: './', // Make assets relative
+
     build: {
         minify: 'terser',
         terserOptions: {
@@ -12,10 +14,22 @@ export default defineConfig({
                 drop_debugger: true,
             },
         },
+
+        
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                dfu: resolve(__dirname, 'dfu.html'),
+                official: resolve(__dirname, 'official.html'),
+                microblocks: resolve(__dirname, 'microblocks.html'),
+            },
+        },
     },
+
     define: {
         APP_VERSION: JSON.stringify(process.env.npm_package_version),
     },
+
     plugins: [
         vue(),
     ],
