c6c20234800ce560fad1a4b77f200717b38c49f4
Dat Tran <dat.tran@ghielectronics.com>
2026-02-09 17:53:32 -0500

    clean

--- src/App.vue
+++ src/App.vue
@@ -57,48 +57,8 @@
 </template>
 
 <script setup>
-import { computed, nextTick, onMounted, reactive, ref, watch } from 'vue';
-import tippy from 'tippy.js';
-import useWebSerial from './js/useWebSerial.js';
-import mitt from "mitt";
 
 
-// Components
-
-
-
-// Const
-const GHI_VID = 0x1B9F;
-const DL_PID = 0xF300;
-const MB_PID = 0xF301;
-// Refs
-
-const $refs = { editor: null, filename: null, input: null, progress: null };
-
-// Emitter
-const emitter = mitt();
-
-// Setup
-const webSerial = useWebSerial($refs, emitter);
-
-
-// Computed
-
-
-// Watch
-
-watch(() => webSerial.isConnected, (value) => {
-    
-});
-
-// Created
-
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    theme.value = 'dark';
-    tippyConfig.theme = 'dark';
-}
-
-// Methods
 
 
 
